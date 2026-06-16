9ae174b7e350d63a018112a4d0c18670d7cf6949
Abhipowar2410 <abhipowar100@gmail.com>
2026-06-16 22:44:22 +0530

    First  Code Only Print

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,6 @@
 print("Hello World")
 
 print("Python")
+print(" Python Data TYpes :NBSC ")
+print("Features:\n 1.Ease to use \n 2.Interpreted Language \n 3.Object Orientend Language ")
+print("Types of Operators :\n ARLBAM \n 1. Arithmetic Operators\n 2. Comparison (Relational) Operators\n 3.Logical Operators")
